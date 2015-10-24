dce93bc32eb4a078001aef16bde6e9acd5b514d7
Brian Karfunkel <bk1051@nyu.edu>
2015-10-24 11:48:14 -0700

    Parse interval

--- bk1051/interval/interval.py
+++ bk1051/interval/interval.py
@@ -3,6 +3,62 @@ Created on Oct 24, 2015
 
 @author: ds-ga-1007
 '''
+import re
+
+def parse_interval(interval_string):
+    """Method to parse a interval string.
+
+    The string should contain two integers, separated by a comma.
+    The first number is the lower bound, the second number the
+    upper bound. The two integers should be enclosed in an opening
+    and closing character, with each being either a bracket--"["/"]"--
+    or a parenthesis--"("/")"; brackets indicate that the bound is
+    inclusive (i.e. that the bound is included in the interval), while
+    parentheses indicate that the bound is exclusive.
+
+    If the interval string cannot be parsed, the method raises an
+    IntervalParseException.
+
+    >>> parse_interval("(0,1)")
+    (0, 1, False, False)
+
+    >>> parse_interval("[0,1]")
+    (0, 1, True, True)
+
+    >>> parse_interval("[-1, 3)")
+    (-1, 3, True, False)
+
+    >>> parse_interval("[-1, 3.5)")
+    Raises exception: needs to be an integer
+    (-1, 3.5, True, False)
+
+    >>> parse_interval("[0 13]")
+    Raises exception: no comma
+
+    >>> parse_interval("{12, 15}")
+    Raises exception: braces instead of valid delimiters
+
+    >>> parse_interval("12, 15")
+    Raises exception, no delimiters
+
+    Note, as shown in following test, that this method does not validate
+    the actual interval (i.e. checking that lower bound is below upper
+    bound); that is done in _validate_interval.
+    >>> _parse_interval("(300, -300]")
+    (300, -300, False, True)
+    """
+    parse_regex = re.compile(r'^\s*([[(])\s*([-+]?\d+)\s*,\s*([-+]?\d+)\s*([])])\s*$')
+    m = parse_regex.match(interval_string)
+    if m:
+        lower_inclusive = m.group(1)=="["
+        lower_bound = int(m.group(2))
+        upper_bound = int(m.group(3))
+        upper_inclusive = m.group(4)=="]"
+        return (lower_bound, upper_bound, lower_inclusive, upper_inclusive)
+
+    else:
+        raise Exception
+
 
 class bound(object):
     def __init__(self, value, is_inclusive):
@@ -25,51 +81,5 @@ class interval(object):
         '''
         Constructor
         '''
-        self.bounds
-        self.inclusive
-
-
-    def _parse_interval(self, interval_string):
-        """Method to parse a interval string.
-
-		The string should contain two integers, separated by a comma.
-		The first number is the lower bound, the second number the
-		upper bound. The two integers should be enclosed in an opening
-		and closing character, with each being either a bracket--"["/"]"--
-		or a parenthesis--"("/")"; brackets indicate that the bound is
-		inclusive (i.e. that the bound is included in the interval), while
-		parentheses indicate that the bound is exclusive.
-
-		If the interval string cannot be parsed, the method raises an
-		IntervalParseException.
-
-		>>> _parse_interval("(0,1)")
-		(0, 1, False, False)
-
-		>>> _parse_interval("[0,1]")
-		(0, 1, True, True)
-
-		>>> _parse_interval("[-1, 3)")
-		(-1, 3, True, False)
-
-		>>> _parse_interval("[-1, 3.5)")
-		Raises exception: needs to be an integer
-		(-1, 3.5, True, False)
-
-		>>> _parse_interval("[0 13]")
-		Raises exception: no comma
-
-		>>> _parse_interval("{12, 15}")
-		Raises exception: braces instead of valid delimiters
-
-		>>> _parse_interval("12, 15")
-		Raises exception, no delimiters
-
-		Note, as shown in following test, that this method does not validate
-		the actual interval (i.e. checking that lower bound is below upper
-		bound); that is done in _validate_interval.
-		>>> _parse_interval("(300, -300]")
-		(300, -300, False, True)
-
-
-		"""
+        #self.bounds
+        #self.inclusive
--- bk1051/interval/tests/test_interval.py
+++ bk1051/interval/tests/test_interval.py
@@ -3,7 +3,10 @@ import interval
 
 class IntervalTestCase(unittest.TestCase):
 
+    def test_parse_interval_0_1_True_True(self):
+        self.assertEqual(interval.parse_interval("[0,1]"), (0,1,True,True))
+
     def test_create_interval_0_1_True_True(self):
         intvl = interval.interval("[0,1]")
         self.assertEqual(intvl.bounds, (0, 1))
-        self.assertEqual(intvl.)
+        self.assertEqual(intvl.inclusive, (True, True))
