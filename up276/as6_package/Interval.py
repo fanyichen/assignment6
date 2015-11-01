6f0fd03d55572c9e588a5ac77ac2409d29fccec8
Urjit Patel <up276@nyu.edu>
2015-11-01 01:19:28 -0500

    commit after updation

--- up276/as6_package/Interval.py
+++ up276/as6_package/Interval.py
@@ -5,82 +5,89 @@ Created on Oct 29, 2015
 '''
 import re
 
+#class to hadle the Exceptions
+class Interval_Exception(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 class Interval:
         
     merge_flag=False  #variable to check whether merge happened for set of intervals or not
     def __init__(self, interval_str):
         
-        #print "str = ",self.str1
-
-        try:
             self.interval_string = interval_str  
             self.validate_string_and_compute_bounds(self.interval_string)
-        except:
-            print "error in entered string"
-
 
     def validate_string_and_compute_bounds(self,interval_str):
         """
-        This function receives the intervals and set the real lower and upper bound
+        This function receives the intervals and compute the real lower and upper bound
         """
-        try:
-            interval_string= interval_str
-            pattern = '(\(|\[)(-?\d+),(-?\d+)(\)|\])'
-            ival_format = False
-            m=re.search(pattern,interval_string)
-            
-            if(m!=None):
-                    
+        interval_string= interval_str
+        pattern = '(\(|\[)(-?\d+),(-?\d+)(\)|\])'    #pattern to check whether received inteval format is correct or not
+        m=re.search(pattern,interval_string)         #compares the user input with the pattern and sets group values
+        
+        #if received input's format is incorrect then value of 'm' will be NULL
+        #so in that case it will directly go into else part, otherwise it will check for values and bounds
+        if(m!=None):
+            try:    
                 self.LowerBound = m.group(2)
                 self.UpperBound = m.group(3)
                 self.LowerBound = int(self.LowerBound)
                 self.UpperBound = int(self.UpperBound)
                 if (m.group(1)=='['):
                     self.LowerBoundInclusive = True
+                    
                 if (m.group(1)=='('):
                     self.LowerBoundInclusive = False
+                   
                 if (m.group(4)==']'):
                     self.UpperBoundInclusive = True
+                   
                 if (m.group(4)==')'):
                     self.UpperBoundInclusive = False
-                
-                
-                if ((self.LowerBoundInclusive == True and self.UpperBoundInclusive == True) 
-                    and (self.LowerBound<=self.UpperBound)):
-                    ival_format = True
+      
+                if ((self.LowerBoundInclusive == True and self.UpperBoundInclusive == True)):
+             
                     self.RealLowerBound = self.LowerBound
                     self.RealUpperBound = self.UpperBound
+                  
                    
-                if ((self.LowerBoundInclusive == True and self.UpperBoundInclusive == False)
-                      and self.LowerBound<self.UpperBound):
-                    ival_format = True
+                if ((self.LowerBoundInclusive == True and self.UpperBoundInclusive == False)):
+                   
                     self.RealLowerBound = self.LowerBound
                     self.RealUpperBound = self.UpperBound-1
                     
-                if (((self.LowerBoundInclusive == False and self.UpperBoundInclusive == True)) 
-                      and self.LowerBound<self.UpperBound):
-                    ival_format = True
+                    
+                if (((self.LowerBoundInclusive == False and self.UpperBoundInclusive == True))):
+                   
                     self.RealLowerBound = self.LowerBound+1
                     self.RealUpperBound = self.UpperBound
+                  
                     
-                if (((self.LowerBoundInclusive == False) and (self.UpperBoundInclusive == False)) and (self.LowerBound<
-    ((self.UpperBound)-1))):
-                    ival_format = True
+                if (((self.LowerBoundInclusive == False) and (self.UpperBoundInclusive == False))):
+                 
                     self.RealLowerBound = self.LowerBound+1
                     self.RealUpperBound = self.UpperBound-1
-        
+             
                 
-            else:
-                print("Bad Format")     
+                if (self.RealLowerBound>self.RealUpperBound):            
+                    raise Interval_Exception("Invalid interval")
+                
+            except:     
+                raise Interval_Exception("Invalid interval")
             
-        
-        except:
-            print "error in Validating string"   
-    
+        else:
+            raise Interval_Exception("Invalid interval")
+            
+ 
     @staticmethod
     def mergeIntervals(interval1, interval2):
         """
         This function receives the set of two intervals and perform the merge if possible
+        It is being called from mergeOverlapping function and returns merged interval 
         """
         try:
             Interval.merge_flag=False
@@ -119,25 +126,24 @@ class Interval:
                 Interval.merge_flag=True
                 NewInterval = Interval("{0}{1},{2}{3}".format(new_lower_bound_character,new_lower_bound,new_upper_bound,new_upper_bound_character))
                 return NewInterval
-            else:
-                raise
-        except:
-       
-            return "bad intervals"
+   
+        except:    
+            raise Interval_Exception("Error in merging two intervals")
             
     @staticmethod
     def mergeOverlapping(intervals):
         """
         This function receives the whole set of intervals and perform the merge by calling mergeInterval 
-        function for set of two intervals
+        function for set of two intervals.This is being called from Insert function and returns the new list 
+        after merging all possible intervals
         """
         try:
-            merging_window_index=0
+            merging_window_index=0        #Local variable which holds the list index for merging the set of intervals one by one
             for i in range(len(intervals)-1):
-                #print i,"th element",intervals[i].str1
+
                 merged_interval=Interval.mergeIntervals(intervals[merging_window_index],intervals[merging_window_index+1])            
-                #print Interval.merge_flag
-                #print"in4"
+                
+                #if Merging occurs for the set of intervals then below code will relace old intervals with the new one in the list
                 if Interval.merge_flag:
                     intervals.pop(merging_window_index)
                     intervals.pop(merging_window_index)
@@ -148,19 +154,18 @@ class Interval:
     
             return intervals
         except:
-            print "error in overlapping intervals"      
+            raise Interval_Exception("Error in merging Overlapping intervals")      
     
-    #@staticmethod
-    #def insert(intervals,new_interval):
-    #    """
-    #    This function receives the whole set of intervals & new interval then 
-    #    append it in the interval list
-    #    """
-    #    try:
-    #        intervals.append(new_interval)
-    #        intervals.sort(key=lambda x: x.RealLowerBound)
-    #        intervals=Interval.mergeOverlapping(new_interval)
-    #        #print "list elements after overlapping"
-    #        for i in range(len(intervals)):
-    #            print intervals[i].str1,
-    #        return intervals
+    @staticmethod
+    def insert(intervals,new_interval):
+        """
+        This function receives the whole set of intervals & new interval then 
+        append it in the interval list. This is being called from main_function and returns new list after 
+        appending new interval and checking for possible merge
+        """
+        try:
+            intervals.append(new_interval)
+            intervals.sort(key=lambda x: x.RealLowerBound)
+            return Interval.mergeOverlapping(intervals)
+        except:
+            raise Interval_Exception("error in inserting interval")
--- up276/as6_package/assignment6.py
+++ up276/as6_package/assignment6.py
@@ -3,56 +3,105 @@ Created on Oct 27, 2015
 
 @author: Urjit Patel - up276
 '''
-#import sys
+
 import re
-import sys
-from Interval import Interval
+
+from Interval import Interval,Interval_Exception
    
 def main_function():
-    try:    
-        str1=raw_input("please enter a list of intervals : ")
-        list_pattern = '([\(|\[]-?\d+,-?\d+[\)|\]])'  
-        list_intervals=re.findall(list_pattern,str1)
-        i=0
-        list_of_objects=[]
-        #print list_intervals
-        for ival in list_intervals:
-                    
-                    interval1=Interval(ival)
-                    list_of_objects.append(interval1)
-                       
-        New_ival=raw_input("Interval ? : ")
-        while(New_ival != 'quit'):
-            interval1=Interval(New_ival)
-            list_of_objects.append(interval1)
-            list_of_objects.sort(key=lambda x: x.RealLowerBound)
-            list_of_objects=Interval.mergeOverlapping(list_of_objects)
-            #print "list elements after overlapping"
-            for i in range(len(list_of_objects)):
-                print list_of_objects[i].interval_string,
-            #list_of_objects=Interval.insert(list_of_objects,interval1)
-            print
-            New_ival=raw_input("Interval ? : ")    
+    '''
+    Body of main_function from where all necessary execution of the program starts.
+    Flow of the Program is as below,
+    main_function() <-> insert(list,new_interval)<->mergeOverlapping(list)<->merge_interval(interval1,interval2)
+    '''
+    try:
+        quit_flag= False    #to check whether user wants to quit the game or not
+        print "INSTRUCTIONS  ::"
+        print "------------------------------------------------------------------------------------------"
+        print "Welcome to Merging the intervals Program. Please note that for the first User input, "
+        print "user can enter as many intervals as he wants. Program will only consider valid intervals "
+        print "from the list. After that user can enter only single interval each time."
+        print "Error will be show for all wrong cases. Enter 'Quit' / 'quit' to exit"    
+        print "------------------------------------------------------------------------------------------"
+        Initial_Interval=raw_input("please enter a list of intervals : ")
+        Initial_Interval = Initial_Interval.strip().replace(' ', '')    #removes all white spaces from user input
         
-        print "Successfully terminated the program...Good Bye...!!!"
-        sys.exit()
-    except:
-        print"error in main function...Plz run the program again and enter valid inputs" 
+        if Initial_Interval in ('quit','Quit','QUIT'):
+            quit_flag=True
+            #print "Successfully terminated the program...Good Bye...!!!"
+            #sys.exit()
+        else:
+            try:
+                list_pattern = '([\(|\[]-?\d+,-?\d+[\)|\]])'               #pattern to find all valid intervals in user input
+                list_intervals=re.findall(list_pattern,Initial_Interval)   #finds all valid interval in initial string       
+                list_of_objects=[]                                         #list containing all valid interval objects
+                #for loop to check all valid intervals and print them
+                for ival in list_intervals:
+                            try:
+                                interval=Interval(ival)                    # Instance creation of Interval class
+                                print ival,
+                                list_of_objects.append(interval)
+                            except Interval_Exception as exception:
+                                print exception,                 
+                print
+            except:    
+                raise Interval_Exception("Invalid interval")    
+        if quit_flag:
+            print "You successfully exited from the program ! See you soon again !"    
+        #to keep asking for new interval untill user quits   
+        while not quit_flag:
+            Input=raw_input("Interval ? : ")
+            Input = Input.strip().replace(' ', '')
+            if Input in ('quit','Quit','QUIT'):
+                quit_flag=True
+            else:
+                try:
+                    interval=Interval(Input)
+                    list_of_objects=Interval.insert(list_of_objects,interval)
+                    
+                    i=0
+                    for i in range(len(list_of_objects)):
+                        print list_of_objects[i].interval_string,
+                    #list_of_objects=Interval.insert(list_of_objects,interval1)
+                    print
+                
+           
+                except Interval_Exception as exception:
+                        print exception   
+            
+            if quit_flag:
+                print "You successfully exited from the program ! See you soon again !"  
+        
+    except Interval_Exception as exception:
+        print "Interval Error:", exception 
 
 if __name__ == "__main__":
-    main_function()
-#except:
-#    print"error in main function...Plz run the program again and enter valid inputs"    
-# To return a new list, use the sorted() built-in function...
-#newlist = sorted(ut, key=lambda x: x.count, reverse=True)
-    
-#print list_of_objects
+    main_function()  #call to main_function()
+"""
 
-#print "Converted list is :", ival.list1
-#ival.mergeintervals([1,5],[3,7])
+Sample Output :-
 
-        #e = sys.exc_info()[0]
-        #write_to_page( "<p>Error: %s</p>" % e )
-#       loop()
-#   except EOFError:
-#       pass
+INSTRUCTIONS  ::
+------------------------------------------------------------------------------------------
+Welcome to Merging the intervals Program. Please note that for the first User input, 
+user can enter as many intervals as he wants. Program will only consider valid intervals 
+from the list. After that user can enter only single interval each time.
+Error will be show for all wrong cases. Enter 'Quit' / 'quit' to exit
+------------------------------------------------------------------------------------------
+please enter a list of intervals : [2,6](9,14](23,29]
+[2,6] (9,14] (23,29]
+Interval ? : (-1,5]
+(-1,6] (9,14] (23,29]
+Interval ? : (9,12)
+(-1,6] (9,14] (23,29]
+Interval ? : wefer
+'Invalid interval'
+Interval ? : (6,2)
+'Invalid interval'
+Interval ? : [3,9]
+(-1,14] (23,29]
+Interval ? : (24,31)
+(-1,14] (23,31)
+Interval ? : quit
+You successfully exited from the program ! See you soon again !
+"""
